4ca90a85cbf97d45af1fe4bade1e6d6e2dff8e31
kledson CR <kledson.rabelo.pb@compasso.com.br>
2022-05-04 11:39:19 -0300

    feat: added funtion async para criar novas tabelas

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,6 +1,19 @@
-const ModeloTabela = require('../rotas/fornecedores/ModeloTabelaFornecedor')
+const modelos = [
+   require('../rotas/fornecedores/ModeloTabelaFornecedor'),
+   require('../rotas/fornecedores/produtos/ModeloTabelaProduto')
+]
 
-ModeloTabela
+async function criarTabelas () {
+   for (let contador = 0; contador < modelos.length; contador++) {
+      const modelo = modelos[contador]
+      await modelo.sync()
+   }
+}
+
+criarTabelas()
+
+//MODELO DE CRIAÇÃO DE TABELA - 1
+/*ModeloTabela
    .sync()
    .then(() => console.log('Tabela criada com sucesso'))
-   .catch(console.log)
+   .catch(console.log)*/
